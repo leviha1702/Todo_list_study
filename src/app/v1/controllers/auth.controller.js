14198cf92c48afc4107e2d0ee94d7bd80438c4f3
diem <levietha170205@gmail.com>
2025-02-05 12:15:48 +0700

    built code new

--- src/app/v1/controllers/auth.controller.js
+++ src/app/v1/controllers/auth.controller.js
@@ -1,12 +1,14 @@
 const AuthService = require("../services/auth.service");
 
 class AuthController{
-    async register(_,res){
+    async register(req,res){
         try{
-            const result= await AuthService.register();
+            const result= await AuthService.register(req.body);
             return res.status(200).json(result);
         } catch (err){
-            return res.status(500).send(err);
+            return res.status(500).send({
+                message:err.message
+            });
         }
     }
 }
--- src/app/v1/services/auth.service.js
+++ src/app/v1/services/auth.service.js
@@ -1,7 +1,11 @@
 const UserModel = require("../models/user.model");
 
 class AuthService{
-    async register(){
+    async register({email, password}){
+        //B1. Check validate email and password
+        if(!email||!password){
+            throw new Error("Email and password are require");
+        }
         return {
             message: "User registered successfully",
         }
